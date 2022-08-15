5464a64594a12534e941586bce8618657f97801f
Andrei <barbutodev@gmail.com>
2022-08-15 14:23:36 -0300

    primeiroC

---
+++ 1p/FP/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int n1=0;
+int n2=1;
+while(n2 <=9){
+    
+    n1+=1;
+    if(n1==11){
+    n1=1;
+    n2+=1;
+    }
+    
+    Console.WriteLine(n2+"x"+n1+"="+n1*n2);
+    
+    }
+

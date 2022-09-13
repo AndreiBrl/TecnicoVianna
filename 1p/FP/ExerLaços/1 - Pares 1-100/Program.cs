874527831d2d1aa6b2e706a3f5b51f7d4118b18e
Andrei <barbutodev@gmail.com>
2022-09-13 14:03:48 -0300

    ExerLaços

---
+++ 1p/FP/ExerLaços/1 - Pares 1-100/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+int x =100;
+for(int i=2;i<=x;i+=2){
+    Console.WriteLine(i);
+}
---
+++ 1p/FP/ExerLaços/3 - Ler número primo/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int x =Convert.ToInt32(Console.ReadLine());
+int cont=1;
+while(cont<x){
+    cont++;
+    if(x/cont==1 ){
+        Console.WriteLine("testando {0}...   é primo",cont);
+        break;
+    }
+    else if(x%cont==0 && x/cont>1){
+        Console.WriteLine("não é primo pq é divisível por {0}",cont);
+        break;
+        
+    }
+}

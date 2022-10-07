901cffcd00853361b834e7f36f11bbcff0e67265
Andrei <barbutodev@gmail.com>
2022-10-07 12:26:20 -0300

    vetores

---
+++ 1p/FP/Vetores/teste/Program.cs
@@ -0,0 +1,18 @@
+﻿int [] vetor = new int[]{1,2,3,6,4,8};
+Console.WriteLine(verificaVetor(vetor,0));
+
+static int verificaVetor(int [] x, int numero){
+
+    for(int i=0;i<x.Length;i++){
+        if(x[i]==numero){
+            Console.WriteLine("Você acertou");
+            return i;
+        }
+       
+    }
+    
+    return -1;
+    
+
+
+}

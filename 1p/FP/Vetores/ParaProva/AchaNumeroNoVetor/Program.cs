ec164972dac479a7346d390c4836ced690d1d540
Andrei <barbutodev@gmail.com>
2022-10-24 17:05:49 -0300

    exer

---
+++ 1p/FP/Vetores/ParaProva/AchaNumeroNoVetor/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] vetor = new int[] {1,2,3,4,5,6,7,7,8};
+int qtdVezes = 0;
+int num = Convert.ToInt32(Console.ReadLine());
+for(int i=0;i<vetor.Length;i++){
+    if(vetor[i]==num){
+        qtdVezes++;
+    }
+}
+if(qtdVezes>0){
+    Console.WriteLine("O numero aparece {0} vezes",qtdVezes);
+}
+else{
+    Console.WriteLine("Não aparece");
+}
---
+++ 1p/FP/Vetores/ParaProva/AchaNumeroNoVetorFuncao/Program.cs
@@ -0,0 +1,12 @@
+﻿int [] vetor = new int [] {1,2,3,4,5,6};
+int num = 4;
+Console.WriteLine(procuraVetor(vetor,num));
+
+static int procuraVetor(int []vetor,int num){
+    for(int i = 0; i<vetor.Length;i++){
+        if(vetor[i]==num){
+            return i;
+        }
+    }
+    return-1;
+}

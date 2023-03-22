01a519baf6dcdd9c726d4c032949be40ad13889c
Andrei <barbutodev@gmail.com>
2023-03-22 12:18:35 -0300

    atua

---
+++ 2p/Estrutura de dados/exer-Ordencao-Insercao/Program.cs
@@ -0,0 +1,32 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        int [] vet = new int []{5,2,4,3,6,1};
+
+        int chave,posDeTras;
+
+
+        for (int i = 1; i < vet.Length; i++)
+        {
+            chave=vet[i];
+            posDeTras=i-1;
+            while(posDeTras>=0 && chave<vet[posDeTras]){
+                vet[posDeTras+1]=vet[posDeTras];
+                posDeTras--;
+
+            }
+            vet[posDeTras+1]=chave;
+
+            
+        }
+
+            for (int i = 0; i < vet.Length; i++)
+            {
+                Console.Write(vet[i]+"-");
+            }
+        }
+
+}

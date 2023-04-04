7ae77d7d133454cdb6e005fe50e8e2109cadac6d
Andrei <barbutodev@gmail.com>
2023-04-04 12:47:40 -0300

    atua

--- 2p/TWA/provaTWA/js/script.js
+++ 2p/TWA/provaTWA/js/script.js
@@ -5,7 +5,7 @@ function tabuada(operador){
     for (let i = 1; i <= 9; i++) {
         conta+=operador+" x "+i+" = "+operador*i+" "+ "\n";
     }
-    console.log(conta);
+
 
     h2.textContent=conta;
     h2.style.width="100px";
@@ -24,6 +24,12 @@ var diasMes = document.querySelectorAll(".mes td");
 
 btn1.addEventListener("click", function(){
     var caixa1=document.querySelector(".caixa1").value;
+
+    diasMes.forEach(element=>{
+        if(element.classList.contains("destaque")){
+            element.classList.remove("destaque");
+        }
+    });
     
     diasMes.forEach(element => {
         
@@ -49,17 +55,46 @@ btn2.addEventListener("click",function(){
 var caixaPrimeiroDia= document.querySelector(".caixaPrimeiroDia").value;
 var caixaSegundoDia=document.querySelector(".caixaSegundoDia").value;
         
+diasMes.forEach(element=>{
+    if(element.classList.contains("destaque")||element.classList.contains("destaque2")||element.classList.contains("destaque3")){
+        element.classList.remove("destaque");
+        element.classList.remove("destaque2");
+        element.classList.remove("destaque3");
+    }
+});
         
     diasMes.forEach(element => {
-
         
+        if(element.innerHTML==parseInt(caixaPrimeiroDia)|| element.innerHTML==parseInt(caixaSegundoDia)){
+            element.classList.add("destaque3");
+        }
 
-        if(element.innerHTML>=parseInt(caixaPrimeiroDia) && element.innerHTML<=parseInt(caixaSegundoDia)){
-            element.classList.add("destaque");
+        if(element.innerHTML>parseInt(caixaPrimeiroDia) && element.innerHTML<parseInt(caixaSegundoDia)){
             
-            console.log(element.innerHTML);
+            element.classList.add("destaque2");
+            
+
         }
 
     });
+
+});
+
+// questão 3
+
+var img = document.querySelector(".questao3 img")
+var contaImagem=1;
+img.style.cursor="pointer";
+img.style.borderRadius="5px";
+img.addEventListener("click", function(){
+    contaImagem++;
+    
+    if(contaImagem>3){
+        contaImagem=1;
+    }
+    console.log(contaImagem);
+    img.src="./img/img"+contaImagem+".jpg";
+
+
 
 });

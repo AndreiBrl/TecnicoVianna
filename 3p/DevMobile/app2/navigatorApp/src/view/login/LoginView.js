227201418be9b089bbc400c1b0d9f466c54cce03
Andrei <barbutodev@gmail.com>
2023-09-18 19:37:42 -0300

    atua

--- 3p/DevMobile/app2/navigatorApp/src/view/login/LoginView.js
+++ 3p/DevMobile/app2/navigatorApp/src/view/login/LoginView.js
@@ -1,25 +1,64 @@
 import { Touchable } from "react-native"
-import { TouchableOpacity, Text, View } from "react-native"
+import { TouchableOpacity, Text, View, StyleSheet } from "react-native"
+import { Avatar, Button, TextInput } from "react-native-paper";
 
 
-const LoginView = ({navigation}) =>{
+const LoginView = ({ navigation }) => {
 
     return (
 
-        <View>
-            <Text>Login</Text>
-            <TouchableOpacity onPress={()=>{navigation.navigate('telaInicial')
-            }}>
-                <Text>Voltar</Text>
+        <View style={estilo.container}>
 
-            </TouchableOpacity>
-            <TouchableOpacity onPress={()=>{navigation.navigate('telaDashboard')
-            }}>
-                <Text>DashBoard</Text>
+            <Avatar.Text  size={50} color={"yellow"} label="XD"
+                style={estilo.logo}
+            />
+            <View style={estilo.body}>
 
-            </TouchableOpacity>
+                <TextInput style={estilo.login}
+                    label="login"
 
+                    placeholder="Informe o  login"
+                />
+                <TextInput style={{backgroundColor:"lightgray"}}
+                    label="senha"
+                    placeholder="Informe o  senha"
+                    right={<TextInput.Icon icon="eye" />}
+                />
+                <Button style={estilo.enviar} icon="plus" mode="contained" onPress={() => console.log('Pressed')}>
+                    Enviar
+
+                </Button>
+            </View>
         </View>
     )
 }
 export default LoginView;
+
+const estilo = StyleSheet.create({
+    Avata:{
+        width:50
+    },
+    container: {
+        flex: 1,
+        alignItems: "center",
+        JustifyContent: "center"
+    },
+    logo: {
+        flex: 0.25,
+        top: 25
+    },
+    body: {
+        flex: 3,
+        justifyContent:"center"
+    
+    },
+    login: {
+        width: 200,
+        marginTop: 50,
+        backgroundColor:"lightgray"
+    },
+    enviar:{
+        marginTop:50
+    }
+
+})
